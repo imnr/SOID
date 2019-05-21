20ab064702298845f6b627fa6ffd755704579d51
imnr <iman.ravakhah@gmail.com>
2019-05-21 23:34:04 +0430

    soid.py updated

--- soid.py
+++ soid.py
@@ -2,15 +2,16 @@ import sys
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('image.jpg')            
-b,g,r = cv2.split(img)               
+img = cv2.imread('image.jpg')       # gets the input (source) image for denoising. "image.jpg" is the directory of the target image      
+b,g,r = cv2.split(img)              # gets the r,g,b values
 source_image = cv2.merge([r,g,b])
 
-dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21) 
+dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21) # Denoising function. the values can be changed to get the best result
 
 b,g,r = cv2.split(dst)                
 output_image = cv2.merge([r,g,b])
 
+#draw input and output
 plt.subplot(211),plt.imshow(source_image)
 plt.subplot(212),plt.imshow(output_image)
 plt.show()
